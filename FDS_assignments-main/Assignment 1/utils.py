0aaa4ea7a8fbb47b7e3688d564a55fc794f3292e
BonoLardinois <78788799+BonoLardinois@users.noreply.github.com>
2022-11-07 14:42:46 +0100

    Add files via upload

---
+++ FDS_assignments-main/Assignment 1/utils.py
@@ -0,0 +1,97 @@
+import re
+from datetime import date
+from os.path import join
+
+import numpy as np
+import pandas as pd
+from nltk.corpus import stopwords
+import nltk
+import pandas as pd
+from nltk.probability import FreqDist
+from nltk import word_tokenize
+from utils import * 
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('vader_lexicon')
+
+
+
+import config as cf
+
+def preprocess(col, steps = ['prep_lower', 
+                             'prep_tokenizewords', 
+                             'prep_drop_sentenceindicators', 
+                             'prep_drop_numbers', 
+                             'prep_drop_stopwords',
+                      'prep_listtosentence']):
+    if 'prep_lower' in steps:
+        col = col.str.lower()
+    if 'prep_tokenizewords' in steps:
+        col = col.apply(word_tokenize)
+    if 'prep_drop_sentenceindicators' in steps:
+        col = col.apply(prep_drop_sentenceindicators)
+    if 'prep_drop_numbers' in steps:
+        col = col.apply(prep_drop_numbers)
+    if 'prep_drop_stopwords' in steps:
+        col = col.apply(prep_drop_stopwords)
+    if 'shorten_texts' in steps:
+        print('not implemented yet')
+    if 'prep_listtosentence' in steps:
+        col = col.apply(lambda x: ' '.join(x))
+        
+    return col
+    
+
+
+def prep_drop_sentenceindicators(col):
+    sentenceindicators = [',', '.', '?', '\t', ':', ';', "''", "'", '"',]
+    col = [word for word in col if word not in sentenceindicators]
+
+    return col
+
+def prep_drop_numbers(s):
+    s = [i for i in s if not i.replace('.', '').isdigit()]
+
+    return s
+
+def prep_drop_stopwords(words, sw=cf.sw):
+    words = [word for word in words if word not in sw]
+    return words
+
+
+# def create_wordcloud():
+#     def cloud(text, filename, stopwords=cf.sw):
+#         # Generate a word cloud image
+#         wordcloud = WordCloud(width=600, height=400, max_font_size=90,
+#                               max_words=50, stopwords=stopwords,
+#                               background_color="white").generate(text)
+
+#         # Display the generated image
+#         plt.imshow(wordcloud, interpolation='bilinear')
+#         plt.axis("off")
+#         plt.show()
+
+#         # Save the wordcloud
+#         wordcloud.to_file(join(cf.path_images, filename))
+
+#         return None
+
+#     print('Generating wordclouds for presentations and Q&A sessions')
+
+#     # Load data
+#     texts = pd.read_pickle(cf.A_B_texts_and_prices_file)
+
+#     # Create word cloud for presentations
+#     text = ""
+#     for t in texts.presentation:
+#         if str(t) != 'nan':
+#             text += ' ' + t
+#     text = re.sub('\n', ' ', text)
+#     cloud(text, "wordcloud_presentations.png")
+
+
+#     print('Finished generating wordclouds for presentations and Q&A sessions')
+
+#     return None
